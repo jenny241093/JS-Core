e19ac16908f87f352dd83ca8942ecc71ddac8e0f
Jenny Zaneva <jeni241093@gmail.com>
2019-01-22 21:02:36 +0200

    Exercise

--- 04.DOCUMENTOBJECTMODEL/04_Register-Notification/solution.js
+++ 04.DOCUMENTOBJECTMODEL/04_Register-Notification/solution.js
@@ -1,22 +1,32 @@
+function register() {
+  let usernameElement = document.getElementById('username');
+  let username = usernameElement.value;
+  let passwordElement = document.getElementById('password');
+  let password = passwordElement.value;
+  let emailElement = document.getElementById('email');
+  let email = emailElement.value;
+  let resultElement = document.getElementById('result');
+  let emailRegex = /(.+)@(.+).(com|bg)/gm;
 
- function register() {
-  let username=document.getElementById('username').value;
-  let email = document.getElementById('email').value;
-  let password = document.getElementById('password').value;
-  let result = document.getElementById("result");
-  let pattern = new RegExp(/(.+)@(.+).(com|bg)/);
-  if (username&&pattern.test(email)!=null&&password) {
-    setTimeout(() => {
-      let titleElement = document.createElement('h1');
-      titleElement.textContent = 'Successful Registration!';
-      result.appendChild(titleElement);
-      result.innerHTML +=`Username: ${username}`;
-      var br = document.createElement("br");
-      result.appendChild(br);
-      result.innerHTML +=`Email: ${email}`;
-      result.appendChild(br);
-      let replacedPass = password.replace(/./g, '*');
-      result.innerHTML +=`Password: ${replacedPass}`;
-    }, (5000));
+  if (email.match(emailRegex) && username !== '' && password !== '') {
+  
+  let h1Element = document.createElement('h1');
+          h1Element.textContent = 'Successful Registration!';
+
+          resultElement.appendChild(h1Element);
+          resultElement.innerHTML += `Username: ${username}`;
+          resultElement.appendChild(document.createElement('br'));
+          resultElement.innerHTML += `Email: ${email}`;
+          resultElement.appendChild(document.createElement('br'));
+          resultElement.innerHTML += `Password: ${('*').repeat(password.length)}`;
+
+          usernameElement.value = '';
+          emailElement.value = '';
+          passwordElement.value = '';
+    
+    
+      setTimeout(() => {
+          resultElement.innerHTML = '';
+      }, 5000);
   }
- }
+}

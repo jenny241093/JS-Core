3e6781b55ec00480e5b18c9d767726839373b06c
Jenny Zaneva <jeni241093@gmail.com>
2019-01-22 20:38:38 +0200

    fd

--- 04.DOCUMENTOBJECTMODEL/04_Register-Notification/solution.js
+++ 04.DOCUMENTOBJECTMODEL/04_Register-Notification/solution.js
@@ -1,4 +1,22 @@
 
  function register() {
-   //TODO
+  let username=document.getElementById('username').value;
+  let email = document.getElementById('email').value;
+  let password = document.getElementById('password').value;
+  let result = document.getElementById("result");
+  let pattern = new RegExp(/(.+)@(.+).(com|bg)/);
+  if (username&&pattern.test(email)!=null&&password) {
+    setTimeout(() => {
+      let titleElement = document.createElement('h1');
+      titleElement.textContent = 'Successful Registration!';
+      result.appendChild(titleElement);
+      result.innerHTML +=`Username: ${username}`;
+      var br = document.createElement("br");
+      result.appendChild(br);
+      result.innerHTML +=`Email: ${email}`;
+      result.appendChild(br);
+      let replacedPass = password.replace(/./g, '*');
+      result.innerHTML +=`Password: ${replacedPass}`;
+    }, (5000));
+  }
  }
--- 04.DOCUMENTOBJECTMODEL/05_Visited-Sites/solution.js
+++ 04.DOCUMENTOBJECTMODEL/05_Visited-Sites/solution.js
@@ -1,3 +1,53 @@
 function solve() {
-  //TODO
+  let buttons = document.getElementsByTagName("a");
+  let spans = document.getElementsByTagName("span");
+  let softuniCount = 1;
+  let googleCount = 2;
+  let youtubeCount = 3;
+  let wikiCount = 4;
+  let gmailCount = 5;
+  let soCount = 6;
+
+  let softuniBtn = buttons[0];
+  let softuniSpan = spans[0];
+  
+
+  softuniBtn.addEventListener("click", function(){
+    softuniCount++;
+    softuniSpan.textContent = "Visited: " +softuniCount + " times";
+  });
+
+  let googleBtn = buttons[1];
+  let googleSPan = spans[1];
+
+  googleBtn.addEventListener("click", function(){
+    googleCount++;
+    googleSPan.textContent = "Visited: " + googleCount + " times";
+  });
+
+  let youtubeBtn = buttons[2];
+  let youtubeSPan = spans[2];
+  youtubeBtn.addEventListener("click", function(){
+    youtubeCount++;
+    youtubeSPan.textContent = "Visited: " + youtubeCount + " times";
+  });
+
+  let wikiBtn = buttons[3]; 
+  let wikiSPan = spans[3];
+  wikiBtn.addEventListener("click", function(){
+    wikiCount++
+    wikiSPan.textContent = "Visited: " + wikiCount + " times";
+  });
+  let gmailBtn = buttons[4];
+  let gmailSPan = spans[4];
+  gmailBtn.addEventListener("click", function(){
+    gmailCount++;
+    gmailSPan.textContent = "Visited: " + gmailCount + " times";
+  });
+  let soBtn = buttons[5];
+  let soSPan = spans[5];
+  soBtn.addEventListener("click", function(){
+    soCount++;
+    soSPan.textContent = "Visited: " +soCount + " times";
+  });
 }

1f8bc443aec24a923d77a9829e09b431815a5e3f
Jenny Zaneva <jeni241093@gmail.com>
2019-01-24 06:12:57 +0200

    homework

--- 05.DocumentObjectModelExercise/02_Chat-Room-with-Pesho/solution.js
+++ 05.DocumentObjectModelExercise/02_Chat-Room-with-Pesho/solution.js
@@ -1,3 +1,39 @@
 function solve() {
- //TODO
+ let myBtn = document.getElementsByTagName('button')[0];
+ let peshoBtn = document.getElementsByTagName('button')[1];
+ let inputFields = Array.from(document.getElementsByTagName('input'));
+ myBtn.addEventListener("click", displayMyMessage);
+ function displayMyMessage() {
+     let myMess = document.getElementById("myChatBox").value;
+     let span = document.createElement("span");
+     let p = document.createElement("p");
+     let div = document.createElement("div");
+     span.textContent = 'Me';
+     p.textContent = myMess;
+
+     div.style.textAlign = 'left';
+     div.appendChild(span);
+     div.appendChild(p);
+     let chronologyDiv = document.getElementById("chatChronology");
+     chronologyDiv.appendChild(div);
+     inputFields[0].value="";
+     
+
+ }
+ peshoBtn.addEventListener("click", displayPeshoMessage);
+ function displayPeshoMessage() {
+    let myMess = document.getElementById("peshoChatBox").value;
+    let span = document.createElement("span");
+    let p = document.createElement("p");
+    let div = document.createElement("div");
+    span.textContent = 'Pesho';
+    p.textContent = myMess;
+
+    div.style.textAlign = 'right';
+    div.appendChild(span);
+    div.appendChild(p);
+    let chronologyDiv = document.getElementById("chatChronology");
+    chronologyDiv.appendChild(div);
+    inputFields[1].value="";
+ }
 }
--- 05.DocumentObjectModelExercise/03_Encode-and-Decode-Messages/solution.js
+++ 05.DocumentObjectModelExercise/03_Encode-and-Decode-Messages/solution.js
@@ -1,3 +1,38 @@
 function solve() {
-	//TODO
+	let buttons = document.getElementsByTagName('button');
+	let encBtn = buttons[0];
+	let decodeBtn = buttons[1];
+	let textAreas = document.getElementsByTagName('textarea');
+
+	encBtn.addEventListener('click',encode);
+
+	function encode() {
+
+		let encodeMessage = textAreas[0].value;
+
+        let newMessage = '';
+
+        encodeMessage.split('').forEach((char) => {
+            let asciiValue = char.charCodeAt(0) + 1;
+            newMessage += String.fromCharCode(asciiValue);
+        });
+
+        textAreas[0].value = "";
+        textAreas[1].value = newMessage;
+		
+	}
+
+	decodeBtn.addEventListener('click',decode);
+
+	function decode() {
+		let decodeMessage = textAreas[1].value;
+        let originalMessage = "";
+
+        decodeMessage.split('').forEach((char) => {
+            let asciiValue = char.charCodeAt(0) - 1;
+            originalMessage += String.fromCharCode(asciiValue);
+        });
+        
+        textAreas[1].value = originalMessage;
+	}
 }

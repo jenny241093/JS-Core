dc8cabef66371aa2842d35aa526b4176812f3a12
Jenny Zaneva <jeni241093@gmail.com>
2019-01-24 08:42:32 +0200

    fff

--- 05.DocumentObjectModelExercise/04_Locked-Profile/solution.js
+++ 05.DocumentObjectModelExercise/04_Locked-Profile/solution.js
@@ -1,3 +1,42 @@
 function solve() {
-   //TODO
-} 
+   let checkLocked = true;
+   let arrRadio = Array.from(document.querySelectorAll('input[type="radio"]'));
+   arrRadio.forEach(rad => {
+       rad.addEventListener('click', currRadio);
+       function currRadio(event) {
+           let radioProfilElem = event.target.parentNode;
+           console.log(radioProfilElem);
+           checkLocked = true
+           if (event.target.value == "unlock") {
+               checkLocked = false;
+           } else if (event.target.value == "lock") {
+               checkLocked = true;
+           };
+       };
+   });
+   let arrBtn = Array.from(document.getElementsByTagName('button'));
+   arrBtn.forEach((btn) => {
+       btn.addEventListener('click', function (event) {
+           let currentProfileElem = event.target.parentNode;
+           console.log(currentProfileElem);
+           let currentInfoElement = currentProfileElem.children[9];
+           console.log(currentInfoElement);
+           let currBtn = currentProfileElem.children[10];
+           console.log(currBtn);
+           let lockedElem = currentProfileElem.children[2];
+           console.log(lockedElem);
+           if (!lockedElem.checked) {
+               checkLocked = false
+           }
+           if (!checkLocked) {
+               if (currBtn.textContent == "Hide it" && !lockedElem.checked) {
+                   currentInfoElement.style.display = "none";
+                   currBtn.textContent = "Show more";
+               } else if (currBtn.textContent == "Show more" && !lockedElem.checked) {
+                   currentInfoElement.style.display = "block";
+                   currBtn.textContent = "Hide it";
+               };
+           };
+       });
+   });
+};
--- 05.DocumentObjectModelExercise/05_Number-Convertor/solution.js
+++ 05.DocumentObjectModelExercise/05_Number-Convertor/solution.js
@@ -1,3 +1,36 @@
 function solve() {
-//TODO
+    
+    
+    let toMenu = document.getElementById('selectMenuTo');
+    let hexaOption = document.createElement("option");
+	hexaOption.value = "hexadecimal";
+	hexaOption.textContent = "Hexadecimal";
+	console.log(hexaOption);
+	toMenu.appendChild(hexaOption);
+    let binaryOption = document.createElement("option");
+	binaryOption.value = "binary";
+	binaryOption.textContent = "Binary";
+	console.log(binaryOption);
+	toMenu.appendChild(binaryOption);
+let button = document.getElementsByTagName('button')[0].
+                      addEventListener('click',clickEvent);
+function clickEvent() {
+    let hexString='';
+    let bin = '';
+    let number = document.getElementById('input').value;
+    console.log('number',number);
+     let selectedOption = document.getElementsByTagName('option')[3].value;
+     console.log(toMenu.value);
+
+     
+     if (toMenu.value='binary') {
+        let bin = (+number).toString(2);
+     }
+     else{
+        hexString = number.toString(16);
+        if (hexString.length % 2) {
+          hexString = '0' + hexString;
+        }
+     }
+ }
 }

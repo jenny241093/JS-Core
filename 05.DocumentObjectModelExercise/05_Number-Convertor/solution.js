d22d009f7b58c5c8e827f555c4d5d113aee33378
Evgeniya <jeni241093@gmail.com>
2019-01-25 07:47:53 +0200

    6done

--- 05.DocumentObjectModelExercise/05_Number-Convertor/solution.js
+++ 05.DocumentObjectModelExercise/05_Number-Convertor/solution.js
@@ -1,36 +1,28 @@
 function solve() {
-    
-    
-    let toMenu = document.getElementById('selectMenuTo');
-    let hexaOption = document.createElement("option");
-	hexaOption.value = "hexadecimal";
-	hexaOption.textContent = "Hexadecimal";
-	console.log(hexaOption);
-	toMenu.appendChild(hexaOption);
-    let binaryOption = document.createElement("option");
-	binaryOption.value = "binary";
-	binaryOption.textContent = "Binary";
-	console.log(binaryOption);
-	toMenu.appendChild(binaryOption);
-let button = document.getElementsByTagName('button')[0].
-                      addEventListener('click',clickEvent);
-function clickEvent() {
-    let hexString='';
-    let bin = '';
-    let number = document.getElementById('input').value;
-    console.log('number',number);
-     let selectedOption = document.getElementsByTagName('option')[3].value;
-     console.log(toMenu.value);
+    let selectMenuTo = document.getElementById('selectMenuTo');
 
-     
-     if (toMenu.value='binary') {
-        let bin = (+number).toString(2);
-     }
-     else{
-        hexString = number.toString(16);
-        if (hexString.length % 2) {
-          hexString = '0' + hexString;
+    let binaryOption = document.createElement('option');
+    binaryOption.value = 'binary';
+    binaryOption.textContent = 'Binary';
+
+    let hexadeicmalOption = document.createElement('option');
+    hexadeicmalOption.value = 'hexadecimal';
+    hexadeicmalOption.textContent = 'Hexadecimal';
+
+    selectMenuTo.appendChild(binaryOption);
+    selectMenuTo.appendChild(hexadeicmalOption);
+
+    document.getElementsByTagName('button')[0].addEventListener('click', clickEvent);
+
+    function clickEvent(e) {
+        let to = Array.from(selectMenuTo.children).filter(x => x.selected)[0];
+        console.log(to);
+        let input = document.getElementById('input').value;
+
+        if (to.value === 'binary') {
+            document.getElementById('result').value = (+input).toString(2);
+        } else if (to.value === 'hexadecimal') {
+            document.getElementById('result').value = (+input).toString(16).toUpperCase();
         }
-     }
- }
+    }
 }
--- 05.DocumentObjectModelExercise/06_SoftUni-Quizz/solution.js
+++ 05.DocumentObjectModelExercise/06_SoftUni-Quizz/solution.js
@@ -1,3 +1,55 @@
 function solve() {
-	//TODO
+    //TODO
+    let rightAnswersCount = 0;
+    let sections = Array.from(document.getElementsByTagName('section'));
+    let firstQuestion = sections[0];
+    console.log(firstQuestion);
+
+    let buttons = Array.from(document.getElementsByTagName('button'));
+    let fistButton = buttons[0];
+    fistButton.addEventListener('click', clickEvent);
+    let secondButton = buttons[1];
+    secondButton.addEventListener('click', clickEvent2);
+    let thirdBtn = buttons[2];
+    thirdBtn.addEventListener('click', clickEvent3);
+
+    function clickEvent() {
+        var radios = document.querySelectorAll('input[name="softUniYear"]:checked');
+        var value = radios.length > 0 ? radios[0].value : null;
+        sections[1].style.display = 'block';
+        if (value == '2013') {
+            rightAnswersCount++;
+        }
+        console.log(radios);
+        console.log(value);
+    }
+
+    function clickEvent2() {
+        var radios = document.querySelectorAll('input[name="popularName"]:checked');
+        var value = radios.length > 0 ? radios[0].value : null;
+        sections[2].style.display = 'block';
+        if (value == 'Pesho') {
+            rightAnswersCount++;
+        }
+        console.log(radios);
+        console.log(value);
+    }
+
+    function clickEvent3() {
+        var radios = document.querySelectorAll('input[name="softUniFounder"]:checked');
+        var value = radios.length > 0 ? radios[0].value : null;
+        sections[2].style.display = 'block';
+        if (value == 'Nakov') {
+            rightAnswersCount++;
+        }
+        console.log(radios);
+        console.log(value);
+        let divRes = document.getElementById('result');
+
+        if (+rightAnswersCount == 3) {
+            divRes.innerHTML = `You are recognized as top SoftUni fan!`;
+        } else {
+            divRes.innerHTML = `You have ${rightAnswersCount} right answers`
+        }
+    }
 }

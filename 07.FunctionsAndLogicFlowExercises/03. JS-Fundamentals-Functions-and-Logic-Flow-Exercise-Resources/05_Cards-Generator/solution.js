dc8377957ecfa4dfaf4f7aa00d9bff3f3fc128e0
Evgeniya Zaneva <Evgeniya.Zaneva@bulpros.com>
2019-01-28 11:53:31 +0200

    fd

--- 07.FunctionsAndLogicFlowExercises/03. JS-Fundamentals-Functions-and-Logic-Flow-Exercise-Resources/05_Cards-Generator/solution.js
+++ 07.FunctionsAndLogicFlowExercises/03. JS-Fundamentals-Functions-and-Logic-Flow-Exercise-Resources/05_Cards-Generator/solution.js
@@ -1,3 +1,22 @@
 function solve() {
-    //TODO...
+    let btn = document.getElementsByTagName('button')[0];
+    btn.addEventListener('click', getCards);
+
+    function getCards() {
+        let from = document.getElementsByTagName('input')[0].value;
+        let to = document.getElementsByTagName('input')[1].value;
+        var selCard = document.getElementsByTagName("select")[0];
+        let arrLenght = +to - +from;
+        let cardSymbol = selCard.options[selCard.selectedIndex].value;
+        for (let i = 0; i < arrLenght; i++) {
+            let div = document.createElement('div').classList.add('card');
+            let firstP = document.createElement('p');
+            let secondP = document.createElement('p');
+            let thirdP = document.createElement('p');
+            firstP.textContent = cardSymbol[cardSymbol.length - 1];
+            thirdP.textContent = cardSymbol[cardSymbol.length - 1];
+
+
+        }
+    }
 }

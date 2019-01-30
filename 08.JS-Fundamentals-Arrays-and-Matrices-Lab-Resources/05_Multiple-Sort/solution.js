fb5377b5bee48ce224e3803513b770ba3c8dc470
Jenny Zaneva <jeni241093@gmail.com>
2019-01-30 09:33:29 +0200

    sort

--- 08.JS-Fundamentals-Arrays-and-Matrices-Lab-Resources/05_Multiple-Sort/solution.js
+++ 08.JS-Fundamentals-Arrays-and-Matrices-Lab-Resources/05_Multiple-Sort/solution.js
@@ -1,3 +1,18 @@
 function solve() {
-  // TODO
+  let listInput = JSON.parse(document.getElementById("arr").value);
+  let sortAscending = [];
+  let sortAlphabetically = [];
+  function sort(input) {
+    let resultContainer = document.getElementById("result");
+    let div1 = document.createElement('div');
+    let div2 = document.createElement('div');
+    sortAscending = input.sort((a, b) => a - b);
+    div1.textContent = sortAscending.join(', ')
+    resultContainer.appendChild(div1);
+    sortAlphabetically = input.sort((a, b) => a.localeCompare(b));
+     div2.textContent = sortAscending.join(', ')
+    resultContainer.appendChild(div2);
+  }
+  sort(listInput);
+
 }

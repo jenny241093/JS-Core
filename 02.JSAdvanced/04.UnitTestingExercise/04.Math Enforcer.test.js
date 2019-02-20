eb2dc9de52cf5d0e4bda9fc85de913a19e40c22c
Evgeniya <jeni241093@gmail.com>
2019-02-20 21:33:38 +0200

    some changes

--- 02.JSAdvanced/04.UnitTestingExercise/04.Math Enforcer.test.js
+++ 02.JSAdvanced/04.UnitTestingExercise/04.Math Enforcer.test.js
@@ -2,89 +2,95 @@ const assert = require('chai').assert;
 const expect = require('chai').expect;
 const mathEnforcer = require('./04.Math Enforcer');
 
-describe('mathEnforcer', function () {
 
-    describe('addFive', function () {
-        it('non-number, return undefined', function () {
-            assert.isUndefined(mathEnforcer.addFive('test'));
+describe('mathEnforcer', function() {
+
+    describe('addFive', function() {
+        it('with a non-number parameter, should return correct result', function() {
+            let input = 'f';
+            let result = mathEnforcer.addFive(input);
+            assert.equal(result, undefined);
         });
 
-        it('positive number, return correct result', function () {
-            assert.equal(mathEnforcer.addFive(5), 10);
+        it('with five will return 10 ', function() {
+            let input = 5;
+            let result = mathEnforcer.addFive(input);
+            assert.equal(result, 10);
         });
-
-        it('negative number, return correct result', function () {
-            assert.equal(mathEnforcer.addFive(-5), 0);
+        it('with five will return 0 ', function() {
+            let input = -5;
+            let result = mathEnforcer.addFive(input);
+            assert.equal(result, 0);
         });
-
-        it('floating point number, return correct result', function () {
-            assert.equal(mathEnforcer.addFive(5.5), 10.5);
+        it('with floating  will return within 0.01 of the correct value ', function() {
+            let input = 3.14;
+            let result = mathEnforcer.addFive(input);
+            assert.equal(result, 8.14);
         });
-
     });
 
-    describe('subtractTen', function () {
-
-        it('non number, return undefined', function () {
-            assert.isUndefined(mathEnforcer.subtractTen('test'));
+    describe('subtractTen', function() {
+        it('with a non-number parameter, should return correct result', function() {
+            let input = 'f';
+            let result = mathEnforcer.subtractTen(input);
+            assert.equal(result, undefined);
         });
-
-        it('positive number, return correct result', function () {
-            assert.equal(mathEnforcer.subtractTen(10), 0);
+        it('with five will return 10 ', function() {
+            let input = 20;
+            let result = mathEnforcer.subtractTen(input);
+            assert.equal(result, 10);
         });
-
-        it('negative number, return correct result', function () {
-            assert.equal(mathEnforcer.subtractTen(-10), -20);
+        it('with five will return 0 ', function() {
+            let input = -5;
+            let result = mathEnforcer.subtractTen(input);
+            assert.equal(result, -15);
         });
-
-        it('float point num, return correct result', function () {
-            expect(mathEnforcer.subtractTen(10.1)).to.be.closeTo(0.1, 0.1);
+        it('with floating  will return within 0.01 of the correct value ', function() {
+            let input = 10.01;
+            let result = mathEnforcer.subtractTen(input);
+            expect(result).to.be.closeTo(0.1, 0.1);
         });
-
-        it('float point num, return correct result', function () {
-            expect(mathEnforcer.subtractTen(20.50)).to.be.closeTo(10.50, 0.1);
+        it('with floating  will return within 0.01 of the correct value ', function() {
+            let input = 20.50;
+            let result = mathEnforcer.subtractTen(input);
+            expect(result).to.be.closeTo(10.50, 0.1);
         });
-
     });
-
-    describe('sum', function () {
-
-        it('non-number num one, return undefined', function () {
-            assert.isUndefined(mathEnforcer.sum('test', 2));
+    describe('sum', function() {
+        it('with a non-number parameter, should return correct result', function() {
+            let input = 'f';
+            let result = mathEnforcer.sum(input);
+            assert.equal(result, undefined);
         });
-
-        it('non-number num two, return undefined', function () {
-            assert.isUndefined(mathEnforcer.sum(2, 'test'));
+        it(' will return 10 ', function() {
+            let num1 = 5;
+            let num2 = 5;
+            let result = mathEnforcer.sum(num1, num2);
+            assert.equal(result, 10);
         });
-
-        it('non number, return undefined', function () {
-            assert.isUndefined(mathEnforcer.sum('bla', 'bla'));
+        it(' will return 7.5 ', function() {
+            let num1 = 2.5;
+            let num2 = 5;
+            let result = mathEnforcer.sum(num1, num2);
+            assert.equal(result, 7.5);
         });
-
-        it('return 4', function () {
-            assert.equal(mathEnforcer.sum(2, 2), 4);
+        it('with floating  will return within 0.01 of the correct value ', function() {
+            let num1 = 3;
+            let num2 = 5.2;
+            let result = mathEnforcer.sum(num1, num2);
+            assert.equal(result, 8.2);
         });
-
-        it('return 5.5', function () {
-            assert.equal(mathEnforcer.sum(2, 3.5), 5.5);
+        it('return 0 ', function() {
+            let num1 = 2;
+            let num2 = -2;
+            let result = mathEnforcer.sum(num1, num2);
+            assert.equal(result, 0);
         });
-
-        it('return 4.8', function () {
-            assert.equal(mathEnforcer.sum(2.8, 2), 4.8);
+        it('return negative value ', function() {
+            let num1 = -2.5;
+            let num2 = -2;
+            let result = mathEnforcer.sum(num1, num2);
+            assert.equal(result, -4.5);
         });
-
-        it('return 20.35', function () {
-            assert.equal(mathEnforcer.sum(10.20, 10.15), 20.35);
-        });
-
-        it('return 0', function () {
-            assert.equal(mathEnforcer.sum(2, -2), 0);
-        });
-
-        it('return -2.5', function () {
-            assert.equal(mathEnforcer.sum(0, -2.5), -2.5);
-        })
-
     });
-
 });

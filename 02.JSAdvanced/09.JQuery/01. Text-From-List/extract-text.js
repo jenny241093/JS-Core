fb0e7e5cbcad63b7f53dcddd04e7da15d0d49883
Jenny Zaneva <jeni241093@gmail.com>
2019-02-28 07:02:31 +0200

    he

--- 02.JSAdvanced/09.JQuery/01. Text-From-List/extract-text.js
+++ 02.JSAdvanced/09.JQuery/01. Text-From-List/extract-text.js
@@ -1,5 +1,4 @@
 function extractText() {
-<<<<<<< HEAD
    let items = $("ul#items li")
      .toArray()
      .map(li => li.textContent)
@@ -7,11 +6,3 @@ function extractText() {
    $("#result").text(items);
  }
  
-=======
-    let result = $('li')
-        .toArray()
-        .map((x) => x.textContent)
-        .join(', ');
-    $('#result').text(result);
-}
->>>>>>> bab0fb2bcae2b3cb3194b5b49ea999b48e380af6

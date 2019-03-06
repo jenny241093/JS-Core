723a986084cd9bed4c1ecc29c012b47dd99962d2
Jenny Zaneva <jeni241093@gmail.com>
2019-03-06 06:29:42 +0200

    commit

---
+++ 02.JSAdvanced/10.JQueryExcercise/02. Timer/timer.js
@@ -0,0 +1,52 @@
+
+function timer() {
+   let isStarted = false;
+   let hours = $('#hours');
+   let minutes = $('#minutes');
+   let seconds = $('#seconds');
+   let startBtn = $('#start-timer');
+   let stopBtn = $('#stop-timer');
+   
+   let hoursCounter = 0;
+   let minutesCounter = 0;
+   let secondsCounter = 0;
+   function incrementCounter() {
+      isStarted=true;
+      secondsCounter++;
+      if (secondsCounter>59) {
+         secondsCounter = 0;
+         minutesCounter++;
+      }else if (minutesCounter>59) {
+         minutesCounter=0;
+         hoursCounter++;
+      }
+   
+      if (hoursCounter<10) {
+         hours.text("0" + hoursCounter.toString());
+      }else{
+         hours.text(hoursCounter.toString());
+      }
+   
+      if (minutesCounter < 10) {
+         minutes.text("0" + minutesCounter.toString());
+      }else {
+         minutes.text(minutesCounter.toString());
+     }
+     if(secondsCounter < 10){
+      seconds.text("0" + secondsCounter.toString());
+   } else {
+      seconds.text(secondsCounter.toString());
+   }
+   }
+   let intervalFunction;
+   startBtn.on("click", () => {
+      if(!isStarted){
+         intervalFunction =  setInterval(incrementCounter, 1000);
+      }
+   });
+   
+   stopBtn.on("click", () => {
+      isStarted = false;
+      clearInterval(intervalFunction);
+   });
+}
